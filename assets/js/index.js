937a07de4d16aa14cf4396181c47ecdc9826b961
João Victor <milk21291br@gmail.com>
2023-02-10 16:30:37 -0300

    javascript adicionado

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,10 @@ const form = document.getElementById('formulario')
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const inputKm = e.target.querySelector('#km');
-    const inputPeso = e.target.querySelector('#peso');
+        const inputKm = document.getElementById('km');
+        const inputPeso = document.getElementById('peso');
+        const resultado = document.getElementById('resultado')
+        const calculo = inputKm * inputPeso * 0.0175
+        resultado.innerHTML = calculo
+        console.log(calculo)
 });
